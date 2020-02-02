dd557d5a6c29a8eb6263c779951eed4e6453ef67
sudoLife <mt.ovinov@gmail.com>
2020-02-02 14:53:14 +0200

    Implement notification

--- Assets/Scripts/InfoText.cs
+++ Assets/Scripts/InfoText.cs
@@ -5,8 +5,27 @@ using UnityEngine.UI;
 
 public sealed class InfoText : MonoBehaviour
 {
+    public static InfoText instance;
+    private int fadeTime = 5;
+Text notification;
+    
+    void Start()
+    {
+        instance = this;
+        notification = GetComponent<Text>();
+        //notification.color.a = 0f;
+    }
     public void ShowMessage(string text)
     {
+        Debug.Log("Started");
+        notification.text = text;
+        StartCoroutine(animate(notification, fadeTime));
+        Debug.Log("Finished");
+    }
 
+    private IEnumerator animate(Text text, int time) {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+        yield return new WaitForSeconds(time);
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
     }
 }
